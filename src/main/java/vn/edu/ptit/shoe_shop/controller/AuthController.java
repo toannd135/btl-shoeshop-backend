2522d574f3feeb735bd4bfd56a78d9d4b2168c38
toannd135 <toannd.b23cn831@stu.ptit.edu.vn>
2026-03-06 19:58:31 +0700

    ok

--- src/main/java/vn/edu/ptit/shoe_shop/controller/AuthController.java
+++ src/main/java/vn/edu/ptit/shoe_shop/controller/AuthController.java
@@ -172,17 +172,17 @@ public class AuthController {
 
     @PostMapping("/verify-otp")
     @ApiMessage("OTP verification")
-    public ResponseEntity<String> otpVerification(@Valid @RequestBody OtpVerificationRequestDTO request) {
+    public ResponseEntity<?> otpVerification(@Valid @RequestBody OtpVerificationRequestDTO request) {
         String response = this.authService.otpVerification(request.getOtp(), request.getEmail());
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(Map.of("message", response));
 
     }
 
     @PostMapping("/reset-password")
     @ApiMessage("Reset password")
-    public ResponseEntity<String> resetPassword(@Valid @RequestBody ResetPasswordRequestDTO request) {
+    public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordRequestDTO request) {
         String response = this.authService.resetPassword(request);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(Map.of("message", response));
     }
 
 }
