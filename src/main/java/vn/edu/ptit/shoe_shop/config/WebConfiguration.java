361b6d92d6fe1df6eb5323be897116a95b5e6531
toannd135 <toannd.b23cn831@stu.ptit.edu.vn>
2026-03-06 22:57:15 +0700

    fix https

--- src/main/java/vn/edu/ptit/shoe_shop/config/WebConfiguration.java
+++ src/main/java/vn/edu/ptit/shoe_shop/config/WebConfiguration.java
@@ -39,7 +39,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList(
-                "https://shoeshopecommerce.dpdns.org",
+                "http://shoeshopecommerce.dpdns.org",
                 "http://localhost:5173/"
         ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
