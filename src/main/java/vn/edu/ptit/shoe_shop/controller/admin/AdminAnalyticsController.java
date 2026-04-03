c4f323d54a40abe3bef91ded684942de63a7c5a0
toannd135 <toannd.b23cn831@stu.ptit.edu.vn>
2026-04-03 22:40:23 +0700

    top selling

--- src/main/java/vn/edu/ptit/shoe_shop/controller/admin/AdminAnalyticsController.java
+++ src/main/java/vn/edu/ptit/shoe_shop/controller/admin/AdminAnalyticsController.java
@@ -6,17 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.edu.ptit.shoe_shop.common.utils.annotation.ApiMessage;
+import vn.edu.ptit.shoe_shop.service.AdminOrderService;
 
 @RestController
 @RequestMapping("/api/v1/admin/analytics")
 @RequiredArgsConstructor
 public class AdminAnalyticsController {
 
-    private final
+    private final AdminOrderService adminOrderService;
 
     @GetMapping("/top-selling-products")
     @ApiMessage("Get top selling products")
     public ResponseEntity<?> getSalesAnalytics() {
-        return ResponseEntity.ok().body(null);
+        return ResponseEntity.ok().body(this.adminOrderService.getTopSellingProducts());
     }
 }
