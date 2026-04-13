23b47eab61b03eb89b664557e66876a41c08ac27
TrungTrung509 <nguyenvinhtrung0509@gmail.com>
2026-04-13 22:30:30 +0700

    fix: cache category

--- src/main/java/vn/edu/ptit/shoe_shop/config/PermissionInterceptor.java
+++ src/main/java/vn/edu/ptit/shoe_shop/config/PermissionInterceptor.java
@@ -1,85 +1,85 @@
-package vn.edu.ptit.shoe_shop.config;
-
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.transaction.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.HandlerMapping;
-import vn.edu.ptit.shoe_shop.common.security.SecurityUtils;
-import vn.edu.ptit.shoe_shop.entity.Permission;
-import vn.edu.ptit.shoe_shop.entity.Role;
-import vn.edu.ptit.shoe_shop.entity.User;
-import vn.edu.ptit.shoe_shop.repository.UserRepository;
-
-import java.nio.file.AccessDeniedException;
-import java.util.List;
-import java.util.UUID;
-
-
-@Component
-public class PermissionInterceptor implements HandlerInterceptor {
-   @Autowired
-   private UserRepository userRepository;
-
-   private static final Logger log = LoggerFactory.getLogger(PermissionInterceptor.class);
-
-   @Override
-   @Transactional
-   public boolean preHandle(HttpServletRequest request,
-                            HttpServletResponse response,
-                            Object handler) throws Exception {
-
-       String path = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
-       String httpMethod = request.getMethod();
-       log.debug("Checking permission for path: {}", path);
-       log.debug("HTTP Method: {}", httpMethod);
-       if (path.startsWith("/api/v1/products") && httpMethod.equals("GET")) {
-           return true;
-       }
-       if (path.startsWith("/api/v1/categories") && httpMethod.equals("GET")) {
-           return true;
-       }
-       if (path.startsWith("/api/v1/coupons/**") && httpMethod.equals("GET")) {
-           return true;
-       }
-
-       // check permission
-       UUID userid = SecurityUtils.getCurrentUserId();
-
-       if (userid != null) {
-           User user = this.userRepository.findByUserId(userid).orElse(null);
-           if (user != null) {
-               Role role = user.getRole();
-               if (role != null) {
-                   List<Permission> permissions = role.getPermissions();
-                   boolean isAllow = permissions.stream().anyMatch(p ->
-                           p.getApiPath().equals(path)
-                                   && p.getMethod().equalsIgnoreCase(httpMethod)
-                   );
-                   if (!isAllow) {
-                       log.warn("Permission DENIED for userId: {} | path: {} | method: {}",
-                               userid, path, httpMethod);
-                       throw new AccessDeniedException("User don't have permission to access this resource");
-                   }
-                   log.info("Permission GRANTED for userId: {} | path: {} | method: {}",
-                           userid, path, httpMethod);
-               } else {
-                   log.error("UserId {} has no role assigned!", userid);
-                   throw new AccessDeniedException("User don't have permission to access this resource");
-               }
-
-           } else {
-               log.warn("User not found in DB for userId: {}", userid);
-           }
-
-       } else {
-           log.debug("No authenticated user or anonymous user");
-       }
-       return true;
-   }
-}
+//package vn.edu.ptit.shoe_shop.config;
+//
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.transaction.Transactional;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.HandlerMapping;
+//import vn.edu.ptit.shoe_shop.common.security.SecurityUtils;
+//import vn.edu.ptit.shoe_shop.entity.Permission;
+//import vn.edu.ptit.shoe_shop.entity.Role;
+//import vn.edu.ptit.shoe_shop.entity.User;
+//import vn.edu.ptit.shoe_shop.repository.UserRepository;
+//
+//import java.nio.file.AccessDeniedException;
+//import java.util.List;
+//import java.util.UUID;
+//
+//
+//@Component
+//public class PermissionInterceptor implements HandlerInterceptor {
+//   @Autowired
+//   private UserRepository userRepository;
+//
+//   private static final Logger log = LoggerFactory.getLogger(PermissionInterceptor.class);
+//
+//   @Override
+//   @Transactional
+//   public boolean preHandle(HttpServletRequest request,
+//                            HttpServletResponse response,
+//                            Object handler) throws Exception {
+//
+//       String path = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+//       String httpMethod = request.getMethod();
+//       log.debug("Checking permission for path: {}", path);
+//       log.debug("HTTP Method: {}", httpMethod);
+//       if (path.startsWith("/api/v1/products") && httpMethod.equals("GET")) {
+//           return true;
+//       }
+//       if (path.startsWith("/api/v1/categories") && httpMethod.equals("GET")) {
+//           return true;
+//       }
+//       if (path.startsWith("/api/v1/coupons/**") && httpMethod.equals("GET")) {
+//           return true;
+//       }
+//
+//       // check permission
+//       UUID userid = SecurityUtils.getCurrentUserId();
+//
+//       if (userid != null) {
+//           User user = this.userRepository.findByUserId(userid).orElse(null);
+//           if (user != null) {
+//               Role role = user.getRole();
+//               if (role != null) {
+//                   List<Permission> permissions = role.getPermissions();
+//                   boolean isAllow = permissions.stream().anyMatch(p ->
+//                           p.getApiPath().equals(path)
+//                                   && p.getMethod().equalsIgnoreCase(httpMethod)
+//                   );
+//                   if (!isAllow) {
+//                       log.warn("Permission DENIED for userId: {} | path: {} | method: {}",
+//                               userid, path, httpMethod);
+//                       throw new AccessDeniedException("User don't have permission to access this resource");
+//                   }
+//                   log.info("Permission GRANTED for userId: {} | path: {} | method: {}",
+//                           userid, path, httpMethod);
+//               } else {
+//                   log.error("UserId {} has no role assigned!", userid);
+//                   throw new AccessDeniedException("User don't have permission to access this resource");
+//               }
+//
+//           } else {
+//               log.warn("User not found in DB for userId: {}", userid);
+//           }
+//
+//       } else {
+//           log.debug("No authenticated user or anonymous user");
+//       }
+//       return true;
+//   }
+//}
--- src/main/java/vn/edu/ptit/shoe_shop/config/PermissionInterceptorConfiguration.java
+++ src/main/java/vn/edu/ptit/shoe_shop/config/PermissionInterceptorConfiguration.java
@@ -1,42 +1,42 @@
-package vn.edu.ptit.shoe_shop.config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class PermissionInterceptorConfiguration implements WebMvcConfigurer {
-   @Bean
-   PermissionInterceptor permissionInterceptor() {
-       return new PermissionInterceptor();
-   }
-
-   @Override
-   public void addInterceptors(InterceptorRegistry registry) {
-       String[] whiteList = {
-               "/",
-               "/api/v1/recommend-products/**",
-               "/api/v1/auth/refresh-token",
-               "/api/v1/auth/login",
-               "/api/v1/auth/register",
-               "/api/v1/auth/verify",
-               "/api/v1/auth/forgot-password",
-               "/api/v1/auth/verify-otp",
-               "/api/v1/auth/reset-password",
-               "/api/v1/auth/oauth2-success",
-               "/api/v1/recommend-products/**",
-               "/storage/**",
-               "/v3/api-docs/**",
-               "/swagger-ui/**",
-               "/swagger-ui.html",
-               "/ws/**",
-               "/ws",
-               "/api/v1/chat/**",
-               "/api/v1/conversations/**"
-       };
-
-       registry.addInterceptor(permissionInterceptor())
-               .addPathPatterns("/**")
-               .excludePathPatterns(whiteList);
-   }
-}
+//package vn.edu.ptit.shoe_shop.config;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class PermissionInterceptorConfiguration implements WebMvcConfigurer {
+//   @Bean
+//   PermissionInterceptor permissionInterceptor() {
+//       return new PermissionInterceptor();
+//   }
+//
+//   @Override
+//   public void addInterceptors(InterceptorRegistry registry) {
+//       String[] whiteList = {
+//               "/",
+//               "/api/v1/recommend-products/**",
+//               "/api/v1/auth/refresh-token",
+//               "/api/v1/auth/login",
+//               "/api/v1/auth/register",
+//               "/api/v1/auth/verify",
+//               "/api/v1/auth/forgot-password",
+//               "/api/v1/auth/verify-otp",
+//               "/api/v1/auth/reset-password",
+//               "/api/v1/auth/oauth2-success",
+//               "/api/v1/recommend-products/**",
+//               "/storage/**",
+//               "/v3/api-docs/**",
+//               "/swagger-ui/**",
+//               "/swagger-ui.html",
+//               "/ws/**",
+//               "/ws",
+//               "/api/v1/chat/**",
+//               "/api/v1/conversations/**"
+//       };
+//
+//       registry.addInterceptor(permissionInterceptor())
+//               .addPathPatterns("/**")
+//               .excludePathPatterns(whiteList);
+//   }
+//}
